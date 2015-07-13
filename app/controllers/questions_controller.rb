2815c5e13b61ab46a6e4e4cbfb33c9dec6838a12
ian bevel & franz knupfer <student@epicodus.com>
2015-07-13 11:58:42 -0700

    display question properties, hide and show form/link

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,24 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+      respond_to do |format|
+        format.html { redirect_to questions_url}
+        format.js
+      end
+    end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :description, :option1, :option2)
   end
 
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,6 @@ describe Question do
   it { should validate_presence_of :option1 }
   it { should validate_presence_of :option2 }
   it { should validate_presence_of :description }
+
+  it { should belong_to :user }
 end
