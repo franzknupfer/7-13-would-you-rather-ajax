f968a50c9963f3a11f489a6b8f290be38d19a57a
Ian Bevel & Franz Knupfer <student@epicodus.com>
2015-07-14 09:45:04 -0700

    program only allows user to vote once, and user must be authenticated to add questions or to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,16 +2,17 @@ class VotesController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
-    vote = @question.votes.new
+    @vote = @question.votes.new
+    @vote.user_id = current_user.id
     selected = params[:selected]
     if selected == "option1"
-      vote.option1 = true
-      vote.vote = true
+      @vote.option1 = true
+      @vote.vote = true
     elsif selected == "option2"
-      vote.option2 = true
-      vote.vote = true
+      @vote.option2 = true
+      @vote.vote = true
     end
-    vote.save
+    @vote.save
     respond_to do |format|
       format.html { redirect_to questions_url}
       format.js
